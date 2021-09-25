92c5696758dc7361959a40998a49af1619909696
Coldiblaster <vbastazin@gmail.com>
2021-09-25 16:54:58 -0300

    feat: add cep promise

--- src/modules/address/infra/http/controllers/CepPromiseController.ts
+++ src/modules/address/infra/http/controllers/CepPromiseController.ts
@@ -1,18 +1,18 @@
-// import { Request, Response } from 'express';
-// import cep from 'cep-promise';
+import { Request, Response } from 'express';
+import cep from 'cep-promise';
 
-// export default class CepPromiseController {
-//   public async index(request: Request, response: Response): Promise<void> {
-//     const { cep_promise } = request.params;
+export default class CepPromiseController {
+  public async index(request: Request, response: Response): Promise<void> {
+    const { cep_promise } = request.params;
 
-//     cep(cep_promise, { timeout: 2000 })
-//       .then((result: any) => {
-//         // retorno dos dados em caso de sucesso
-//         return response.json(result);
-//       })
-//       .catch((error: any) => {
-//         // retorno do erro caso exista
-//         return response.status(400).json(error);
-//       });
-//   }
-// }
+    cep(cep_promise, { timeout: 2000 })
+      .then((result: any) => {
+        // retorno dos dados em caso de sucesso
+        return response.json(result);
+      })
+      .catch((error: any) => {
+        // retorno do erro caso exista
+        return response.status(400).json(error);
+      });
+  }
+}
--- src/modules/address/infra/http/routes/cepPromise.routes.ts
+++ src/modules/address/infra/http/routes/cepPromise.routes.ts
@@ -1,22 +1,19 @@
-// import { ensureAuthenticated } from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-// import { celebrate, Joi, Segments } from 'celebrate';
-// import { Router } from 'express';
-// import CepPromiseController from '../controllers/CepPromiseController';
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import CepPromiseController from '../controllers/CepPromiseController';
 
-// const cepPromiseController = new CepPromiseController();
+const cepPromiseController = new CepPromiseController();
 
-// const addressRouter = Router();
+const addressRouter = Router();
 
-// addressRouter.use(ensureAuthenticated());
+addressRouter.get(
+  '/:cep_promise',
+  celebrate({
+    [Segments.PARAMS]: {
+      cep_promise: Joi.string().required(),
+    },
+  }),
+  cepPromiseController.index
+);
 
-// addressRouter.get(
-//   '/:cep_promise',
-//   celebrate({
-//     [Segments.PARAMS]: {
-//       cep_promise: Joi.string().required(),
-//     },
-//   }),
-//   cepPromiseController.index,
-// );
-
-// export default addressRouter;
+export default addressRouter;
