6d6d4b6e8b671d45da0cc1218742527e0cc3eb1a
Coldiblaster <vbastazin@gmail.com>
2021-09-26 23:17:06 -0300

    refactor: remove router

--- src/shared/infra/routes/index.ts
+++ src/shared/infra/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
 import addressRouter from '../../../modules/address/infra/http/routes/address.routes';
-import searchZipCodeRouter from '../../../modules/address/infra/http/routes/cepPromise.routes';
 
 const routes = Router();
 
 routes.use('/address', addressRouter);
-routes.use('/search-zip-code', searchZipCodeRouter);
 
 export default routes;
