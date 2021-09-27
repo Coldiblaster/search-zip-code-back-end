034000b2630e54875327fac8bed6ad295a745ea1
Coldiblaster <vbastazin@gmail.com>
2021-09-26 23:21:23 -0300

    feat: add request via cep

--- src/modules/address/services/CreateAddressService.ts
+++ src/modules/address/services/CreateAddressService.ts
@@ -1,18 +1,24 @@
 import { injectable, inject } from 'tsyringe';
+import axios from 'axios';
 
 import AppError from '../../../shared/errors/AppError';
+import { handleZipCodeFormat } from '../../../shared/utils';
 
 import IAddressRepository from '../repositories/IAddressRepository';
 
 import Address from '../infra/typeorm/entities/Address';
 
-interface IRequest {
+interface IResponse {
   bairro: string;
   cep: string;
   uf: string;
   logradouro: string;
   complemento?: string;
   localidade: string;
+}
+
+interface IRequest {
+  zipCode: string;
 }
 
 @injectable()
@@ -22,27 +28,35 @@ class CreateAddressService {
     private addressRepository: IAddressRepository
   ) {}
 
-  public async execute({
-    bairro,
-    cep,
-    localidade,
-    logradouro,
-    complemento,
-    uf,
-  }: IRequest): Promise<Address> {
-    const checkForZipCode = await this.addressRepository.findByZipCode(cep);
+  public async execute({ zipCode }: IRequest): Promise<Address> {
+    const cepFormatted = handleZipCodeFormat(zipCode);
+
+    const checkForZipCode = await this.addressRepository.findByZipCode(
+      cepFormatted
+    );
 
     if (checkForZipCode) {
       throw new AppError('Existing zip code.');
     }
 
+    const url = `https://viacep.com.br/ws/${cepFormatted}/json/`;
+
+    const { bairro, localidade, logradouro, uf, complemento } = await axios
+      .get<IResponse>(url)
+      .then(response => {
+        return response.data;
+      })
+      .catch(() => {
+        throw new AppError('Zip code not found in the base of the ViaCEP.');
+      });
+
     const address = await this.addressRepository.create({
       bairro,
-      cep,
+      cep: cepFormatted,
       localidade,
       logradouro,
-      complemento,
       uf,
+      complemento,
     });
 
     return address;
