09023b4ee6c76ce2d3eaa9b52b9d7769f294fe00
Coldiblaster <vbastazin@gmail.com>
2021-09-26 23:18:12 -0300

    feat: add zipCode formatted

--- src/modules/address/services/ShowSingleAddressService.ts
+++ src/modules/address/services/ShowSingleAddressService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 
+import { handleZipCodeFormat } from '../../../shared/utils';
 import AppError from '../../../shared/errors/AppError';
 import IAddressRepository from '../repositories/IAddressRepository';
 import Address from '../infra/typeorm/entities/Address';
@@ -16,10 +17,12 @@ class ShowSingleAddress {
   ) {}
 
   public async execute({ cep }: IRequest): Promise<Address | undefined> {
-    const address = await this.addressRepository.findByZipCode(cep);
+    const cepFormatted = handleZipCodeFormat(cep);
+
+    const address = await this.addressRepository.findByZipCode(cepFormatted);
 
     if (!address) {
-      throw new AppError('Address not found.');
+      throw new AppError('Address not found.', 204);
     }
 
     return address;
