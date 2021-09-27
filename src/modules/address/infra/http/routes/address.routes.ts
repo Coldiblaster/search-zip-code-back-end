00e7df407b111e3b96faec476769c28e7f313459
Coldiblaster <vbastazin@gmail.com>
2021-09-26 23:20:49 -0300

    refactor: removing comments

--- src/modules/address/infra/http/routes/address.routes.ts
+++ src/modules/address/infra/http/routes/address.routes.ts
@@ -6,59 +6,10 @@ const addressController = new AddressController();
 
 const addressRouter = Router();
 
-addressRouter.post(
-  '/',
-  celebrate({
-    [Segments.BODY]: {
-      cep: Joi.number().required(),
-      bairro: Joi.string().required(),
-      logradouro: Joi.string().required(),
-      uf: Joi.string().required(),
-      complemento: Joi.string().empty('').default('default value'),
-      localidade: Joi.string().required(),
-    },
-  }),
-  addressController.create
-);
-
-// addressRouter.put(
-//   '/:endereco_id',
-//   celebrate({
-//     [Segments.PARAMS]: {
-//       endereco_id: Joi.string().uuid().required(),
-//     },
-//     [Segments.BODY]: {
-//       bairro: Joi.string().required(),
-//       cep: Joi.number().required(),
-//       rua: Joi.string().required(),
-//       numero: Joi.string().required(),
-//       complemento: Joi.string().empty('').default('default value'),
-//       cidade_id: Joi.string().uuid().required(),
-//     },
-//   }),
-//   addressController.update
-// );
-
-// addressRouter.delete(
-//   '/:endereco_id',
-//   celebrate({
-//     [Segments.PARAMS]: {
-//       endereco_id: Joi.string().uuid().required(),
-//     },
-//   }),
-//   addressController.delete
-// );
+addressRouter.post('/', addressController.create);
 
 addressRouter.get('/', addressController.show);
 
-addressRouter.get(
-  '/:cep',
-  celebrate({
-    [Segments.PARAMS]: {
-      cep: Joi.number().required(),
-    },
-  }),
-  addressController.index
-);
+addressRouter.get('/:cep', addressController.index);
 
 export default addressRouter;
